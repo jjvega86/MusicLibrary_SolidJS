ed9543da73cb42c2b42dff2f2b166b052b6545de
JJ <jj.vega86@gmail.com>
2022-07-31 21:32:21 -0400

    Wrote service functions for songs

---
+++ services/songService.js
@@ -0,0 +1,58 @@
+const { Song } = require("../models/song");
+
+const getSongs = async () => {
+  try {
+    let songs = await Song.find();
+    return songs;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getSong = async (songId) => {
+  try {
+    let song = await Song.findById(songId);
+    return song;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const createSong = async (songData) => {
+  try {
+    let song = new Song(songData);
+    let { error } = song.songValidate(songData);
+    if (error) throw new Error("Song body not valid!");
+    return song;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const updateSong = async (songId, songData) => {
+  try {
+    let song = await Song.findByIdAndUpdate(songId, songData, {
+      new: true,
+    });
+    return song;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const deleteSong = async (songId) => {
+  try {
+    let song = await Song.findByIdAndDelete(songId);
+    return song;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = {
+  getSongs,
+  getSong,
+  createSong,
+  updateSong,
+  deleteSong,
+};
